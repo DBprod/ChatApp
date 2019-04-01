fc465f6117c495aeed32283ba53d6ad03d59f1fb
Winkel Yin <winkel515@hotmail.com>
2019-04-01 17:03:39 -0400

    changed message activity layout

--- app/src/main/java/com/example/chatapp/MessageActivity.java
+++ app/src/main/java/com/example/chatapp/MessageActivity.java
@@ -3,6 +3,7 @@ package com.example.chatapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +19,7 @@ import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -46,7 +48,7 @@ public class MessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message);
         FirebaseApp.initializeApp(this);
 
-        editMessage = (EditText) findViewById(R.id.editMessageE);
+        editMessage = (EditText) findViewById(R.id.editMessage);
 
         receiver_name = getIntent().getExtras().getString("receiverName");
         receiver_uid = getIntent().getExtras().getString("uid");
@@ -96,10 +98,35 @@ public class MessageActivity extends AppCompatActivity {
             receiverPost.child("receiver").setValue(receiver_name);
             receiverPost.child("chatId").setValue(mUser.getUid());
             receiverPost.child("sender").setValue(0);
-
-
-            mMessageList.scrollToPosition(mMessageList.getAdapter().getItemCount()); // sets the recycler view to last message
         }
+
+        mReceiverRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                mMessageList.smoothScrollToPosition(mMessageList.getAdapter().getItemCount());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
