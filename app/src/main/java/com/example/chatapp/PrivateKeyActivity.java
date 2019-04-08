f8ec1e8779aa292c810f2930924cbebe9331e9bd
danielbucci <danielbucci2@gmail.com>
2019-04-08 10:22:43 -0400

    adjustments made to PrivateKeyActivity.xml

--- app/src/main/java/com/example/chatapp/PrivateKeyActivity.java
+++ app/src/main/java/com/example/chatapp/PrivateKeyActivity.java
@@ -3,13 +3,23 @@ package com.example.chatapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class PrivateKeyActivity extends AppCompatActivity {
+
+    private TextView privateKeyText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_key);
+
+        privateKeyText = (TextView) findViewById(R.id.privateKeyTextView);
+
+
+
+
     }
 
     public void copyTextBtnClicked(View view){
